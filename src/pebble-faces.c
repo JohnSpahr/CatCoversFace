21a8628fc49d49afbd39b0a45c5d980c4d5e67e2
JohnS <37165173+JohnSpahr@users.noreply.github.com>
2022-04-27 18:33:12 -0400

    Add files via upload

--- src/pebble-faces.c
+++ src/pebble-faces.c
@@ -5,19 +5,35 @@ static Window *window;
 static TextLayer *text_layer;
 static BitmapLayer *bitmap_layer;
 static GBitmap *current_bmp;
+static TextLayer *s_time_layer; //the actual clock
 
 static char *images[] = {
-  "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/cherie.png",
-  "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/mtole.png",
-  "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/chris.png",
-  "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/heiko.png",
-  "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/thomas.png",
-  "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/matt.png",
-  "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/katharine.png",
-  "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/katherine.png",
-  "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/alex.png",
-  "http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-faces/lukasz.png"
+  "https://github.com/JohnSpahr/CatCovers/blob/master/images/color/cande.PNG?raw=true",
+  "https://github.com/JohnSpahr/CatCovers/blob/master/images/color/republic.PNG?raw=true",
+  "https://github.com/JohnSpahr/CatCovers/blob/master/images/color/violator.PNG?raw=true",
+  "https://github.com/JohnSpahr/CatCovers/blob/master/images/color/wish.PNG?raw=true"
 };
+
+
+
+static void update_time() {
+  // Get a tm structure
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+                                          "%H:%M" : "%I:%M", tick_time);
+
+  // Display this time on the TextLayer
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
+  update_time();
+}
+
 
 static unsigned long image = 0;
 
@@ -46,19 +62,40 @@ static void window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Shake it!");
+
+  text_layer_set_text(text_layer, "Shake to pick a cover!");
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
   bitmap_layer = bitmap_layer_create(bounds);
   layer_add_child(window_layer, bitmap_layer_get_layer(bitmap_layer));
   current_bmp = NULL;
+
+
+  //create clock text layer...
+  s_time_layer = text_layer_create(
+      GRect(0, 140, bounds.size.w, bounds.size.h));
+
+  //various setup stuff...
+  text_layer_set_background_color(s_time_layer, GColorBlack);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+  //add clock text layer to window layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+
 }
 
 static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
   bitmap_layer_destroy(bitmap_layer);
   gbitmap_destroy(current_bmp);
+
+
+  // Destroy TextLayer
+  text_layer_destroy(s_time_layer);
 }
 
 void download_complete_handler(NetDownload *download) {
@@ -99,6 +136,13 @@ static void init(void) {
   window_stack_push(window, true);
 
   accel_tap_service_subscribe(tap_handler);
+
+
+update_time();
+  // Register with TickTimerService
+tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+
 }
 
 static void deinit(void) {
@@ -110,4 +154,6 @@ int main(void) {
   init();
   app_event_loop();
   deinit();
+
+
 }
